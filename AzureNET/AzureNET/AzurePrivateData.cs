adfb9539e43f7062652195a909723c01fd05560a
krzysiekj94 <k.jerzynski1@gmail.com>
2019-01-06 15:00:59 +0100

    - add comment to AzurePrivateData.cs

--- AzureNET/AzureNET/AzurePrivateData.cs
+++ AzureNET/AzureNET/AzurePrivateData.cs
@@ -8,6 +8,7 @@ namespace AzureNET
 {
     static class AzurePrivateData
     {
+        //TODO insert your data below
         public const string FETCH_TOKEN_URI  = "";
         public const string REGION_STRING = "westeurope";
         public const string SUBSCRIPTION_KEY = "";
